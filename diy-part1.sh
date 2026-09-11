f61e4198c1666d85b9e60d31cdeeeac16862663f
chao-afk <864907886@qq.com>
2026-09-12 00:28:07 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -4,24 +4,16 @@
 # File name: diy-part1.sh
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
 
-## 1. 添加 QModem 5G模组软件源到 feeds.conf.default
-sed -i '$a src-git qmodem https://github.com/FUjr/QModem.git;main' feeds.conf.default
-
-## 2. 获取 Modem‑Manager‑Webui 静态网页，无需编译
+# 2. 获取 Modem-Manager-Webui 静态网页，无需编译
 # 创建固件files目录，最终固件内路径 /www/webui
 rm -rf files/www/webui
 mkdir -p files/www
-
 # 临时目录拉取仓库
 rm -rf temp_webui
 mkdir -p temp_webui
 git clone --depth 1 https://github.com/panasonic850218/Webui temp_webui
-
-# 仓库里 web/ 文件夹才是真正静态网页，复制到固件目录
+# 仓库里 web/ 文件才是真正静态网页，复制到固件目录
 cp -r temp_webui/web/* files/www/webui/
-
 # 清理临时下载目录
 rm -rf temp_webui
